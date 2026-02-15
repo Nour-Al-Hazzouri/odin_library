0297cbc236c5180139083027fff55c8b2aa24a75
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2026-02-15 06:33:18 +0200

    Initial Table Structure and Data Entry
    Created the initial table structure in HTML
    Created the initial JS logic to input data into table
    Used the .map and .join method to add a new column for each array item

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello, world!");
+const myLibrary = [];
+
+function Book(id, title, author, year, read) {
+  // the constructor...
+  if (!new.target) {
+    throw Error("Ensure to use 'new' when creating instances.");
+  }
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.read = read;
+}
+
+function addBookToLibrary(id, title, author, year, read) {
+  // take params, create a book then store it in the array
+  const book = new Book(id, title, author, year, read);
+  myLibrary.push(book);
+}
+
+addBookToLibrary(crypto.randomUUID(), "theHobbit", "JD. Vance", 2010, "Yes");
+addBookToLibrary(crypto.randomUUID(), "theHobbit", "JD. Vance", 2010, "Yes");
+addBookToLibrary(crypto.randomUUID(), "theHobbit", "JD. Vance", 2010, "Yes");
+
+const myTable= document.querySelector("#myTable tbody");
+myTable.innerHTML = myLibrary.map(book => `<tr><td>${book.id}</td><td>${book.title}</td><td>${book.author}</td><td>${book.year}</td><td>${book.read}</td></tr>`).join('');
