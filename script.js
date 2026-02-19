478c371e8bd782de07c21160ba21d499114512df
Nour-Al-Hazzouri <nour4lhazzouri@gmail.com>
2026-02-19 14:22:42 +0200

    Initial complete logic to add books to table

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Array to store books
 const myLibrary = [];
 
-function Book(id, title, author, year, read) {
+// Object to use to create new books
+function Book(id, title, author, year) {
   // the constructor...
   if (!new.target) {
     throw Error("Ensure to use 'new' when creating instances.");
@@ -9,18 +11,30 @@ function Book(id, title, author, year, read) {
   this.title = title;
   this.author = author;
   this.year = year;
-  this.read = read;
 }
 
-function addBookToLibrary(id, title, author, year, read) {
+// Function to add new created books into the library array
+function addBookToLibrary(id, title, author, year) {
   // take params, create a book then store it in the array
-  const book = new Book(id, title, author, year, read);
+  const book = new Book(id, title, author, year);
   myLibrary.push(book);
 }
 
-addBookToLibrary(crypto.randomUUID(), "theHobbit", "JD. Vance", 2010, "Yes");
-addBookToLibrary(crypto.randomUUID(), "theHobbit", "JD. Vance", 2010, "Yes");
-addBookToLibrary(crypto.randomUUID(), "theHobbit", "JD. Vance", 2010, "Yes");
+// Mock books created to test functionality through function call
+addBookToLibrary(crypto.randomUUID(), "Harry Potter", "Trump", 2000);
+addBookToLibrary(crypto.randomUUID(), "theHobbit", "JD. Vance", 2010);
+addBookToLibrary(crypto.randomUUID(), "William James", "Hillary Clinton", 1998);
 
-const myTable= document.querySelector("#myTable tbody");
-myTable.innerHTML = myLibrary.map(book => `<tr><td>${book.id}</td><td>${book.title}</td><td>${book.author}</td><td>${book.year}</td><td>${book.read}</td></tr>`).join('');
+// Query to have access over the tbody
+const tBody= document.querySelector('#data');
+
+// The initial function to add each table row
+function bookTable(bookElement) {
+  for (let i= 0; i < bookElement.length; i++) {
+    const tr= document.createElement('tr');
+    const transformedBook= `<td>${bookElement[i].id}</td><td>${bookElement[i].title}</td><td>${bookElement[i].author}</td><td>${bookElement[i].year}</td>`
+    tr.innerHTML= transformedBook;
+    tBody.appendChild(tr);
+  }
+}
+bookTable(myLibrary)
