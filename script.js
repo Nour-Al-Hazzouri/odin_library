b46f55268b525fa5f6c9d453c81912de9fa6d0bf
Nour Al Hazzouri <127341856+Nour-Al-Hazzouri@users.noreply.github.com>
2026-03-24 05:48:57 +0200

    Complete Functionality (#1)
    * Initial Table Structure and Data Entry
    
    Created the initial table structure in HTML
    Created the initial JS logic to input data into table
    Used the .map and .join method to add a new column for each array item
    
    * Initial complete logic to add books to table
    
    * Update script and html to receive user input
    
    Updated the html file to include a dialog that opens and closes to receive user input
    Updated the script to receive user input in order to add a book each time the form is submitted
    
    * Added a reset button for the form
    
    * Add Book for Each Row
    
    Added the ability to add a new book object for each row
    The submitted form data is turned into a new FormData object then into a raw JS object
    The FormData transformed object is added into the library then added as a new row
    Next: Add delete and change status functionality.
    
    * Complete Delete Function
    
    Completed the delete function in the table
    This was done by locating the row using data attributes
    The delete button shares the same data-delete value as its parent row
    When clicked using an event listener, deleteRow is triggered to remove row
    
    * Complete Read Status Changer Logic
    
    Completed the logic responsible for changing the read status logic.
    Now every time a new book is added, functions will be executed to keep status changing functional.
    eventExecution function will be called on book addition or buttons clicked
    
    * Improved Deletion Logic to Include myLibrary Array
    
    * Complete Functionality With Further Styling Improvements To Be Implemented

--- script.js
+++ script.js
@@ -1,1 +1,108 @@
-console.log("Hello, world!");
+// Array to store books
+const myLibrary = [];
+
+// Counter for displaying added book in table
+let booksAdded = 0;
+
+// Object to create new books
+function Book(title, author, year, read) {
+  if (!new.target) {
+    throw Error("Ensure to use 'new' when creating instances.");
+  }
+  const UUID = crypto.randomUUID();
+  this.id = UUID;
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.read = read;
+}
+
+// Function to add new created books into the library array
+function addBookToLibrary(title, author, year, read) {
+  const book = new Book(title, author, year, read);
+  myLibrary.push(book);
+}
+
+// Receive user inputted books
+const dialog = document.querySelector("#form-dialog");
+const form = document.querySelector("form");
+form.addEventListener("submit", extractBook);
+function extractBook(e) {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  const formData = Object.fromEntries(data.entries());
+  addBookToLibrary(
+    formData.title,
+    formData.author,
+    formData.year,
+    formData.read,
+  );
+  addBookToTable(myLibrary);
+}
+
+// Function to add each table row
+const tBody = document.querySelector("#data");
+function addBookToTable(bookElement) {
+  const tr = document.createElement("tr");
+  tr.setAttribute("data-id", `${bookElement[booksAdded].id}`);
+  const transformedBook = `<td>${bookElement[booksAdded].title} 
+  <button data-delete>Delete</button></td>
+  <td>${bookElement[booksAdded].author}</td>
+  <td>${bookElement[booksAdded].year}</td>
+  <td>${bookElement[booksAdded].read} <button data-read>Change</button></td>`;
+  tr.innerHTML = transformedBook;
+  tBody.appendChild(tr);
+  booksAdded += 1;
+  eventExecution();
+}
+
+// Create the deleteRow function
+function deleteRow(e) {
+  const selectedRow = e.target.closest("tr").getAttribute("data-id");
+  const rowToDelete = document.querySelector(`[data-id="${selectedRow}"]`);
+  let deleteFromLibrary;
+  for (let book of myLibrary) {
+    if (book.id == selectedRow) {
+      deleteFromLibrary = myLibrary.indexOf(book);
+    }
+  }
+  myLibrary.splice(deleteFromLibrary, 1);
+  rowToDelete.remove();
+}
+
+// Prototype method to change read status
+Book.prototype.changeRead = function () {
+  if (this.read == "Yes") {
+    this.read = "No";
+  } else {
+    this.read = "Yes";
+  }
+};
+
+// Create function to change read status in table
+function changeReadStatus(e) {
+  const target = e.target.closest("tr").getAttribute("data-id");
+  let readStatus;
+  for (let book of myLibrary) {
+    if (book.id == target) {
+      book.changeRead();
+      readStatus = book.read;
+    }
+  }
+  const selectedButton = e.target;
+  selectedButton.closest("td").innerHTML =
+    `${readStatus} <button data-read>Change</button>`;
+  eventExecution();
+}
+
+// Logic to add event listener to each button in the table
+function eventExecution() {
+  const buttons = document.querySelectorAll("tbody button");
+  buttons.forEach((button) => {
+    if (button.hasAttribute("data-delete")) {
+      button.addEventListener("click", deleteRow);
+    } else if (button.hasAttribute("data-read")) {
+      button.addEventListener("click", changeReadStatus);
+    }
+  });
+}
